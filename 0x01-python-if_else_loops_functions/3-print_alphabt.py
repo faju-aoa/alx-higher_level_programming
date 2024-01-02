1eda2505510c80b300044f15f917af93a9e862c3
faju-aoa <unugorfelix@gmail.co>
2024-01-02 15:39:40 -0800

    fourth commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for asc in range(97, 123):
-    if 'q' not in chr(asc) and not in 'e' chr(asc):
+    if 'q' not in chr(asc) and 'e' not in chr(asc):
         print("{}".format(chr(asc)), end="")

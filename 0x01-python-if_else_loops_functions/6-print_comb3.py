44ed1466479b731c059f6838080fc8a9f0ff9772
faju-aoa <unugorfelix@gmail.co>
2024-01-03 16:47:34 -0800

    seventh commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for l in range(0, 9):
     for p in range(l+1, 10):
         if l == 8 and p == 9:
-            print("{}{}".format(l, p))
+            print("89")
         else:
             print("{}{}".format(l, p), end=", ")

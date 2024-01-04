73140022cce7a98334fe083072c292e8f3cdb84c
faju-aoa <unugorfelix@gmail.co>
2024-01-03 17:29:41 -0800

    9-commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if chr(ord(str)) >= 65 and chr(ord(str)) <= 90:
+        if chr(ord(i)) >= 65 and chr(ord(i)) <= 90:
             print("{}".format(str), end="")
         print()

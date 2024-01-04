a5abc3ec7e3c4dd73336dce1f0c1c7f82ef7479d
faju-aoa <unugorfelix@gmail.co>
2024-01-04 11:20:35 -0800

    0-commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "main":
     from add_0 import add
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-    Returns:
-        The return value. a + b
-    """
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))

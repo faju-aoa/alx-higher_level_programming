cf7f061338a02d7c0fab2141e470ca87b505a51e
faju-aoa <unugorfelix@gmail.co>
2024-01-06 15:23:20 -0800

    0-commit

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def print_list_integer(my_list=[]):
-        my_list = [1, 2, 3, 4, 5]
-        for i in my_list:
-            print("{:d}".format(i))
+def print_list_integer(my_list=[]):
+    my_list = [1, 2, 3, 4, 5]
+    for i in my_list:
+        print("{:d}".format(i))

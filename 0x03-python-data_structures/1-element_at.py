c66f82f3f4ec5eebf8f6a77c9433db36fecea727
faju-aoa <unugorfelix@gmail.co>
2024-01-08 00:17:49 -0800

    1-commit

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
   g= [i for i in my_list]
-  if idx < 0 or idx > len(g):
+  if idx < 0:
+    return None
+  elif idx > len(g):
     return None
   else:
    return g[idx]

65ac89112ed1859d898d346ebb9f79ac77b65b5d
faju-aoa <unugorfelix@gmail.co>
2024-01-10 00:16:07 -0800

    3-commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     total_num = 0
     for i in range(len(sys.argv)-1):
         total_num += int(sys.argv[i + 1])
-        print("{}".format(total_num))
+    print("{}".format(total_num))

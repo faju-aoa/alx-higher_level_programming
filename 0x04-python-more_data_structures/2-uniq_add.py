d2bbcb9fb7540da5c57e66d02a15797bd40c72a8
faju-aoa <unugorfelix@gmail.co>
2024-01-12 00:09:02 -0800

    2-commit

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     unique = []
-    add_uniq =0
+    add_uniq = 0
     for numbers in my_list:
-      if numbers not in unique:
-        unique.append(numbers)
+        if numbers not in unique:
+            unique.append(numbers)
     for i in range(0, len(unique)):
-      add_uniq+= unique[i]
+        add_uniq += unique[i]
     return add_uniq

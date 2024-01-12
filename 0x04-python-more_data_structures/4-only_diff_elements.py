ddc8b4023e1ef1d6203345570b1e1fd0c2aca16c
faju-aoa <unugorfelix@gmail.co>
2024-01-12 08:17:41 -0800

    4-commit

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    if not set_1:
-        return set_1
-    if not set_2:
-        return set_2
     diff_set = set_1 ^ set_2
     return diff_set

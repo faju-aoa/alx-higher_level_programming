76d26baf0263f9f95483ecdf0e68882907ddad0a
akpeme21 <unugorfelix@gmail.com>
2024-01-23 13:19:36 -0800

    3-commit

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,6 @@
 def safe_print_division(a, b):
     try:
         result = a/b
-        print("Inside result: {}".format(result))
     except(ValueError, ZeroDivisionError):
         result = None
     finally:

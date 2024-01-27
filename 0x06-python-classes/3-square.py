4c61691cbe8a3f6e7cb98022a69d24985558e21a
akpeme21 <unugorfelix@gmail.com>
2024-01-27 02:38:46 -0800

    2-commit

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a square class"""
+
+
+class Square:
+    """representing the square class"""
+
+    def __init__(self, size=0):
+        """Initializing this square class
+        Args: size - represnets the size of the square class
+        Raises:TypeError: if size is not integer
+        ValueError: if size is less than zero"""
+        if isinstance(size, int):
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Area of a circle method"""
+        return self.__size * self.__size

19043304fd23040e9c8d759b3d8a8dffa0900702
akpeme21 <unugorfelix@gmail.com>
2024-01-27 01:04:19 -0800

    1-commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a square class"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size)
+        """Initializing this square class
+        Args: size - represnets the size of the square class"""
+
+        self.__size = size
